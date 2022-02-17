4cc85588d1fe3ba891c60552e7f04b9698c34017
omariam <37511728+o-mariam@users.noreply.github.com>
2022-02-17 18:34:31 +0200

    ok

--- data.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-
-
-data=pd.read_csv('dataset.csv')
-data.drop('Poster_Link','Released_Year','Certificate','Runtime','Genre',IMDB_Rating,Overview,Meta_score,Director,Star1,Star2,Star3,Star4,No_of_Votes,Gross)
